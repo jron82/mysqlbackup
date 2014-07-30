780d773b38f7786fbb633a9132f32ae1c08a9c88
Ronald Jordan <jron@gmx.com>
2014-07-29 23:58:20 -0700

    Removed FTP Options
    I don’t need these options

--- backup.sh
+++ backup.sh
@@ -15,11 +15,7 @@ MUSER="MYSQL_USER"
 MPASS="MYSQL_USER_PASSWORD"
 MHOST="localhost"
 
-### FTP server Setup ###
-FTPD="YOUR_FTP_BACKUP_DIR"
-FTPU="YOUR_FTP_USER"
-FTPP="YOUR_FTP_USER_PASSWORD"
-FTPS="YOUR_FTP_SERVER_ADDRESS"
+
 
 ######DO NOT MAKE MODIFICATION BELOW#####
 #########################################
@@ -50,29 +46,10 @@ do
           do
                 FILE=$BACKUP/$NOW/$db/$i.sql.gz
                     echo $i; $MYSQLDUMP --add-drop-table --allow-keywords -q -c -u $MUSER -h $MHOST -p$MPASS $db $i | $GZIP -9 > $FILE
-                      done
-                    done
-
-                    ### Compress all tables in one nice file to upload ###
-
-                    ARCHIVE=$BACKUP/$NOW.tar.gz
-                    ARCHIVED=$BACKUP/$NOW
-
-                    $TAR -cvf $ARCHIVE $ARCHIVED
-
-                    ### Dump backup using FTP ###
-                    cd $BACKUP
-                    DUMPFILE=$NOW.tar.gz
-                    $FTP -n $FTPS <<END_SCRIPT
-                    quote USER $FTPU
-                    quote PASS $FTPP
-                    cd $FTPD
-                    mput $DUMPFILE
-                    quit
-                    END_SCRIPT
-
-                    ### Delete the backup dir and keep archive ###
-
-                    rm -rf $ARCHIVED
-          done
+                     
+             done
 done
+
+                    
+
+
